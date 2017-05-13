16083e2ebfd0b10bc88b04c1f702813b6762c990
Ecoila <eangel.Beltranc@gmail.com>
2017-05-13 10:05:24 -0400

    eric

---
+++ operadorIgualdad.js
@@ -0,0 +1,6 @@
+var x=10;
+var y="10";
+console.log("si el valor de x="+(x));
+console.log("si el valos de y=\""+y+"\"");
+console.log("en tonces los valores de x==y seran iguales : "+(x==y));
+console.log("por tanto los valores de x y de y son real mente iguales x===y:"+(x===y));
